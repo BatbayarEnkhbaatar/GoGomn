449c0dd622fce0fae5f0d2111038f1644a406531
Baggy <baggy@spiderkim.com>
2022-03-12 18:51:32 +0900

    version running on AWS with lambda function 2022.3.12

--- gogo_crawler/crawler.py
+++ gogo_crawler/crawler.py
@@ -2,10 +2,14 @@ from selenium import webdriver
 from datetime import datetime
 import time
 import uuid
-import ikonmn.constant.db as db
-import ikonmn.constant.constants as const
+import constant.db as db
+import constant.constants as const
 import re
 from datetime import date
+import os
+import boto3
+
+client = boto3.client("lambda")
 
 
 def cleanhtml(raw_html):
@@ -13,6 +17,11 @@ def cleanhtml(raw_html):
     cleantext = re.sub(cleanr, '', raw_html)
     cleantext = cleantext.replace("&nbsp;", "").replace("·", "")
     return cleantext
+
+
+date_today = date.today()
+current_date = datetime.now().strftime('%Y-%m-%d %H:%M')
+time_terms = const.gogo_date_terms.terms
 
 
 def check_published_date(found_date):
@@ -35,27 +44,29 @@ def check_published_date(found_date):
 
 def lambda_handler(event, context):
     ####chrome driver
+    #  Set time zone
+    os.environ['TZ'] = 'Asia/Ulaanbaatar'
+    time.tzset()
     options = webdriver.ChromeOptions()
+    options.binary_location = '/opt/headless-chromium'
     options.add_argument('--headless')
-    options.add_argument('disable-gpu')
-    options.add_argument('start-maximized')
-    options.add_argument('disable-infobars')
-    options.add_argument('--disable-extensions')
-    options.add_experimental_option('excludeSwitches', ['enable-automation'])
-    driver = webdriver.Chrome("../driver/chromedriver", options=options)
+    options.add_argument('--no-sandbox')
+    options.add_argument('--single-process')
+    options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome('/opt/chromedriver', chrome_options=options)
     target_url = "https://gogo.mn"
     driver.get(target_url)
     driver.implicitly_wait(30)
     print("Page has been fully loaded")
     jobs = []
     time.sleep(5)
-    date_today = date.today()
+
     link_num = 20
     start_link_num = 1
     div_class = "back-blue uk-text-bold text-white text-16 uk-display-inline-block padding-10 uk-text-uppercase padding-left-20 padding-right-20 margin-top-40 margin-bottom-40"
     gogoapp = "gogoapp"
     loadMoreXpath = "/html/body/div[2]/div[2]/section[2]/div/div/div[1]/div/div[4]/a/div"
-    time_terms = const.gogo_date_terms.terms
+
     scrape_info = db.get_last_scrapped("gogoapp")
 
     sincewhen = datetime.strptime(scrape_info, '%Y-%m-%d %H:%M')
@@ -75,7 +86,7 @@ def lambda_handler(event, context):
         status = "today's_new"
         link = get_att.get_attribute('href')
         istoday = check_published_date(pdate_2)[0]
-        current_date = datetime.now().strftime('%Y-%m-%d %H:%M')
+
         published_date = check_published_date(pdate_2)[1]
         if istoday == True:
             db.put_items_crawling_results(str(job_id), status, link, str(published_date))
@@ -94,5 +105,11 @@ def lambda_handler(event, context):
         link_num += 8
     driver.close()
     driver.quit()
+    # str(i) + " pages crawled successfully, " + str(current_date)
+    response = client.invoke(
+        FunctionName='arn:aws:lambda:ap-northeast-2:906743063735:function:_gogo_scapper',
+        InvocationType='RequestResponse',
+    )
 
-lambda_handler("key1", "value1",)
+    return "gogomn crawling has been done successfully!"
+
